12c29f94482955f1b46fd87afde0fa508534a104
DINHHUAN <huanden246@gmail.com>
2025-04-23 12:14:51 +0700

    initial backend

---
+++ src/test/java/com/workspace/llmsystem/LlmsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workspace.llmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
